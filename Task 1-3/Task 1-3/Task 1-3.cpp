bb4740a0314a1a108aea83c35e1d12d922afa5ba
Boris Zinchenko <Digstiks@gmail.com>
2021-04-02 15:11:54 +0300

    Работа 1-3

---
+++ Task 1-3/Task 1-3/Task 1-3.cpp
@@ -0,0 +1,29 @@
+﻿// Task 1-3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+double getPressure(const int g, const double Weight,
+    const double Area)
+    {
+    return (Weight * g) / Area;
+    }
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int g=9.81;
+    double Weight;
+    cout << "Введите массу бетонной плиты" << endl;
+    cin >> Weight;
+    double Area;
+    cout << "Введите площадь опоры" << endl;
+    cin >> Area;
+    cout << "Давление: ";
+    double Pressure = getPressure(g, Weight, Area); //Вызов функции
+    cout << Pressure;
+    return 0;
+}
+
